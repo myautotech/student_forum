64698bf5f5c1d421da0ef17f0f77b5412d7245fd
Sudarshan Dhokale <sudarshandhokale09@gmail.com>
2015-07-21 00:04:48 +0530

    datatable changes done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,9 @@
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require dataTables/jquery.dataTables
+//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
+//= require dataTables/extras/dataTables.responsive
+//= require dataTables/jquery.dataTables.foundation
 //= require jquery.validate
 //= require jquery.validate.additional-methods
 //= require ckeditor/init
